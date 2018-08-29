f9c34623ea206a07d2c5dbb43ac35914a0fa2091
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-08-29 17:27:53 +0200

    end of day commit

--- src/main/java/co/simplon/footteam/model/Player.java
+++ src/main/java/co/simplon/footteam/model/Player.java
@@ -7,14 +7,75 @@ public class Player {
 	 * - First name of the player
 	 * - Last name of the player
 	 * - Position on the field (number)
-	 * - Average note (0 to 100)
+	 * - Average avgNote (0 to 100)
 	 */
+	
+	private String firstName;
+	private String lastName;
+	private String position;
+	private int avgNote;
+
 	
 	/*
 	 * Code constructors here
 	 * - One empty constructor
 	 * - One constructor with all attributes
 	 */
+	public Player() {
+		super();
+	}
+	
+	public Player(String firstName, String lastName, String position, int avgNote) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.position = position;
+		this.avgNote = avgNote;
+	}
+
+	/* 
+	 * Getters and setters
+	 */
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public int getAvgNote() {
+		return avgNote;
+	}
+
+	public void setAvgNote(int avgNote) {
+		this.avgNote = avgNote;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Player [firstName=" + firstName + ", lastName=" + lastName + ", position=" + position + ", avgNote="
+				+ avgNote + "]";
+	}
+	
 	
 	
 }
--- src/main/java/co/simplon/footteam/model/Team.java
+++ src/main/java/co/simplon/footteam/model/Team.java
@@ -1,5 +1,8 @@
 package co.simplon.footteam.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Team {
 
 	/*
@@ -9,22 +12,67 @@ public class Team {
 	 * - Players list
 	 */
 	
+	private String name;
+	private TeamType type;
+	private Set<Player> players = new HashSet<Player>();
+
 	/*
 	 * Code constructors here
 	 * - One empty constructor
 	 * - One constructor with all attributes
 	 */
 	
+	public Team() {
+		super();
+	}
+
+	public Team(String name, TeamType type, Set<Player> players) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.players = players;
+	}
+	
 	/*
 	 * Code here a function that sort players by their notes
 	 */
+	
+	public Set<Player> sortPlayersByAvgNote() {
+		Set<Player> res =  new HashSet<Player>();
+		return res;
+	}
 	
 	/*
 	 * Code here a function that sort players by their position
 	 */
 	
+	public Set<Player> sortPlayersByPosition() {
+		Set<Player> res =  new HashSet<Player>();
+		return res;
+	}
+	
 	/*
 	 * Code here a function that sort players by their name
 	 */
+	
+	public Set<Player> sortPlayersByName() {
+		Set<Player> res =  new HashSet<Player>();
+		return res;
+	}
+	
+	/**
+	 * Add a player to the team
+	 * @param player: player to add
+	 * @return true if player was not already part of the team, false otherwise
+	 */
+	public boolean addPlayer(Player player) {
+		return this.players.add(player);
+	}
+	
+	@Override
+	public String toString() {
+		return "Team [name=" + name + ", type=" + type + ", nb players=" + players.size() + "]";
+	}
+	
 	
 }
